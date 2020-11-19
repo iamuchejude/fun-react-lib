4b1db51469a7aedab521a3bd293941b1961d59b9
Uche Jude <iamuchejude@gmail.com>
2020-11-19 02:45:09 +0100

    use createElement() in heading component

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,29 +1,7 @@
-import React from 'react';
-
+import { createElement } from 'react';
 import PropTypes from 'prop-types';
 
-const switchHeading = (type, children) => {
-  switch (type) {
-    case 'h1':
-      return <h1>{children}</h1>;
-    case 'h2':
-      return <h2>{children}</h2>;
-    case 'h3':
-      return <h3>{children}</h3>;
-    case 'h4':
-      return <h4>{children}</h4>;
-    case 'h5':
-      return <h5>{children}</h5>;
-    case 'h6':
-      return <h6>{children}</h6>;
-    default:
-      return <h2>{children}</h2>;
-  }
-};
-
-const Heading = ({ type, children }) => {
-  return <>{switchHeading(type, children)}</>;
-};
+const Heading = ({ type, children }) => createElement(type, {}, children);
 
 Heading.propTypes = {
   type: PropTypes.oneOfType(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
